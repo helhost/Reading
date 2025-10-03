22570a54cfab27f0766159a2cd9753d1aacae7f1
Halvor Etterdal <halvoret@stud.ntnu.no>
2025-10-03 12:20:07 +0100

    removed % after health-check

--- server/api.go
+++ server/api.go
@@ -21,7 +21,7 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
     return
   }
   w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-  w.Write([]byte("up"))
+  w.Write([]byte("up\n"))
 }
 
 func listBooksHandler(db *sql.DB) http.HandlerFunc {
